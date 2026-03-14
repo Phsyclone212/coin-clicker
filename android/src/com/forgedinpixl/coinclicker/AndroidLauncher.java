3360431a426e6c5206b5a520cdd011a183a39f32
Phsyclone212 <phsyclone212@gmail.com>
2026-03-13 21:01:36 -0400

    Initial LibGDX project setup

---
+++ android/src/com/forgedinpixl/coinclicker/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.forgedinpixl.coinclicker;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.forgedinpixl.coinclicker.CoinClicker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinClicker(), config);
+	}
+}
---
+++ desktop/src/com/forgedinpixl/coinclicker/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.forgedinpixl.coinclicker;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.forgedinpixl.coinclicker.CoinClicker;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("coin-clicker");
+		new Lwjgl3Application(new CoinClicker(), config);
+	}
+}
